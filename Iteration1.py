a853593eaecbd55cf38c3f4ec626ad47bac7c88b
SambodhS <118314865+SambodhS@users.noreply.github.com>
2022-11-19 11:37:38 -0500

    Update Iteration1.py

--- Iteration1.py
+++ Iteration1.py
@@ -1,77 +1,78 @@
 import random
 
+matrixSize = 5 # gets you a 5x5 matrix
+iterations = 5
+pU = 0.25
+pD = 0.25
+pL = 0.25
+pR = 0.25
+ 
+class App :
+    def  generateMatrix() :
+        mat = [[0] * (matrixSize) for _ in range(matrixSize)]
+        mat[random.randint(1, matrixSize - 1)][random.randint(1, matrixSize - 1)] = 1
+        return mat
 
-def  generateMatrix() :
-    mat = [[0] * (5) for _ in range(5)]
-    mat[random.randint(1, 4)][random.randint(1, 4)] = 1
-    return mat
+    def  getAgentPosition( mat) :
+        agentPosition = [0] * (2)
+        i = 0
+        while (i < len(mat)) :
+            j = 0
+            while (j < len(mat[i])) :
+                if (mat[i][j] == 1) :
+                    agentPosition[0] = i
+                    agentPosition[1] = j
+                j += 1
+            i += 1
+        return agentPosition
 
-def  getAgentPosition( mat) :
-    agentPosition = [0] * (2)
-    i = 0
-    while (i < len(mat)) :
-        j = 0
-        while (j < len(mat[i])) :
-            if (mat[i][j] == 1) :
-                agentPosition[0] = i
-                agentPosition[1] = j
-            j += 1
-        i += 1
-    return agentPosition
+    def  moveAgent( mat,  agentPosition) :
+        r = random.uniform(0, 1)
+        if (r < pU) :
+            if (agentPosition[0] != 0) :
+                mat[agentPosition[0] - 1][agentPosition[1]] = 1
+       print(“Up”)
+            else :
+                mat = App.moveAgent(mat, agentPosition)
+        elif(pU < r and r <= pU + pD) :
+            if (agentPosition[0] != 4) :
+                mat[agentPosition[0] + 1][agentPosition[1]] = 1
+       print(“Down”)
+            else :
+                mat = App.moveAgent(mat, agentPosition)
+        elif(pU + pD < r and r <= pU + pD + pL) :
+            if (agentPosition[1] != 0) :
+                mat[agentPosition[0]][agentPosition[1] - 1] = 1
+       print(“Left”)
+            else :
+                mat = App.moveAgent(mat, agentPosition)
+        elif(pU + pD + pL < r and r <= pU + pD + pL + pR) :
+            if (agentPosition[1] != 4) :
+                mat[agentPosition[0]][agentPosition[1] + 1] = 1
+      print(“Right”)
+            else :
+                mat = App.moveAgent(mat, agentPosition)
+        return mat
 
-def  moveAgent( mat,  agentPosition) :
-    pU = 0.25
-    pD = 0.25
-    pL = 0.25
-    pR = 0.25
-    r = random.uniform(0, 1)
-    if (r < pU) :
-        if (agentPosition[0] != 0) :
-            mat[agentPosition[0] - 1][agentPosition[1]] = 1
-   print(“Up”)
-        else :
+    def print2D( mat) :
+        for row in mat :
+            print(row)
+    @staticmethod
+    def main( args) :
+        mat = App.generateMatrix()
+        # trajectory = [[0] * (3) for _ in range(6)]
+        App.print2D(mat)
+        print("")
+        for x in range(iterations):
+            agentPosition = App.getAgentPosition(mat)
+            # print("[" + str((x + 1)) + ", " + str(agentPosition[0]) + ", " + str(agentPosition[1]) + "]")
+            # trajectory[x][0] = x + 1
+            # trajectory[x][1] = agentPosition[0]
+            # trajectory[x][2] = agentPosition[1]
+            mat[agentPosition[0]][agentPosition[1]] = 0
             mat = App.moveAgent(mat, agentPosition)
-    elif(pU < r and r <= pU + pD) :
-        if (agentPosition[0] != 4) :
-            mat[agentPosition[0] + 1][agentPosition[1]] = 1
-   print(“Down”)
-        else :
-            mat = App.moveAgent(mat, agentPosition)
-    elif(pU + pD < r and r <= pU + pD + pL) :
-        if (agentPosition[1] != 0) :
-            mat[agentPosition[0]][agentPosition[1] - 1] = 1
-   print(“Left”)
-        else :
-            mat = App.moveAgent(mat, agentPosition)
-    elif(pU + pD + pL < r and r <= pU + pD + pL + pR) :
-        if (agentPosition[1] != 4) :
-            mat[agentPosition[0]][agentPosition[1] + 1] = 1
-  print(“Right”)
-        else :
-            mat = App.moveAgent(mat, agentPosition)
-    return mat
-
-def print2D( mat) :
-    for row in mat :
-        print(row)
-@staticmethod
-def main( args) :
-    mat = App.generateMatrix()
-    # trajectory = [[0] * (3) for _ in range(6)]
-    App.print2D(mat)
-    print("")
-    time = 0
-    while (time < 5) :
-        agentPosition = App.getAgentPosition(mat)
-        # print("[" + str((time + 1)) + ", " + str(agentPosition[0]) + ", " + str(agentPosition[1]) + "]")
-        # trajectory[time][0] = time + 1
-        # trajectory[time][1] = agentPosition[0]
-        # trajectory[time][2] = agentPosition[1]
-        mat[agentPosition[0]][agentPosition[1]] = 0
-        mat = App.moveAgent(mat, agentPosition)
-        time += 1
-    print("")
-    App.print2D(mat)
+        print("")
+        App.print2D(mat)
 
 
 
