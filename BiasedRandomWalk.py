d66d46fe2406e86709c703654ebab2e070c0a093
SambodhS <118314865+SambodhS@users.noreply.github.com>
2022-12-16 17:33:06 -0500

    Add files via upload

--- BiasedRandomWalk.py
+++ BiasedRandomWalk.py
@@ -3,8 +3,7 @@ import random
 import math
 
 matrixSize = 21  # gets you a 21x21 matrix
-iterations = 10
-runtimes = []
+iterations = 1
 xPoints = []
 yPoints = []
 
@@ -15,7 +14,7 @@ class App:
         mat = [[0] * matrixSize for _ in range(matrixSize)]
         # mat[random.randint(1, matrixSize - 1)][random.randint(1, matrixSize - 1)] = 1
         # mat[math.floor(matrixSize / 2)][math.floor(matrixSize / 2)] = 1
-        mat[20][0] = 1
+        mat[matrixSize - 1][0] = 1
         return mat
 
     def getAgentPosition(mat):
@@ -37,49 +36,50 @@ class App:
             if agentPosition[0] != 0:
                 mat[agentPosition[0] - 1][agentPosition[1]] = 1
             else:
-                App.moveAgent(mat, agentPosition, 0, 0.33, 0.33, 0.33)
+                App.moveAgent(mat, agentPosition, 0, 1/4, 1/4, 1/2)
         elif pU < r <= pU + pD:
             if agentPosition[0] != matrixSize - 1:
                 mat[agentPosition[0] + 1][agentPosition[1]] = 1
             else:
-                App.moveAgent(mat, agentPosition, 0.33, 0, 0.33, 0.33)
+                App.moveAgent(mat, agentPosition, 1/4, 0, 1/2, 1/4)
         elif pU + pD < r <= pU + pD + pL:
             if agentPosition[1] != 0:
                 mat[agentPosition[0]][agentPosition[1] - 1] = 1
             else:
-                App.moveAgent(mat, agentPosition, 0.33, 0.33, 0, 0.33)
+                App.moveAgent(mat, agentPosition, 1/4, 1/2, 0, 1/4)
         elif pU + pD + pL < r <= pU + pD + pL + pR:
             if agentPosition[1] != matrixSize - 1:
                 mat[agentPosition[0]][agentPosition[1] + 1] = 1
             else:
-                App.moveAgent(mat, agentPosition, 0.33, 0.33, 0.33, 0)
+                App.moveAgent(mat, agentPosition, 1/2, 1/4, 1/4, 0)
         return mat
 
     def print2D(mat):
         for row in mat:
             print(row)
 
-    def plot(runtime, beta):
-        xPoints.append(beta)
-        yPoints.append(runtime)
+    def plot(agentPosition):
+        xPoints.append(agentPosition[1])
+        yPoints.append(-agentPosition[0])
         plt.scatter(xPoints, yPoints)
 
     @staticmethod
     def main():
-        for count in range(iterations):
+        for _ in range(iterations):
             mat = App.generateMatrix()
             # App.print2D(mat)
-            runtime = 0
             agentPosition = App.getAgentPosition(mat)
-            while agentPosition != [0, 20]:
+            runtime = 0
+            while agentPosition != [0, matrixSize - 1]:
                 agentPosition = App.getAgentPosition(mat)
                 mat[agentPosition[0]][agentPosition[1]] = 0
-                mat = App.moveAgent(mat, agentPosition, 0.25, 0.25, 0.25, 0.25)
+                mat = App.moveAgent(mat, agentPosition, 1/4, 1/4, 1/4, 1/4)
                 runtime = runtime + 1
                 agentPosition = App.getAgentPosition(mat)
+                # print(str(agentPosition[0]) + ", " + str(agentPosition[1]) + ", " + str(runtime))
+            # print("")
             # App.print2D(mat)
             print(runtime)
-        plt.show()
 
 
 if __name__ == "__main__":
