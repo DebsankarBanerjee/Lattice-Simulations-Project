afaeda4943f49bffcd0aac9a5938789ec95213b0
SambodhS <118314865+SambodhS@users.noreply.github.com>
2022-12-17 09:10:01 -0500

    Add files via upload

--- BiasedRandomWalk.py
+++ BiasedRandomWalk.py
@@ -1,11 +1,11 @@
-import matplotlib.pyplot as plt
 import random
-import math
 
 matrixSize = 21  # gets you a 21x21 matrix
-iterations = 1
-xPoints = []
-yPoints = []
+iterations = 100
+beta = 10  # bias towards a certain direction
+
+b = (1 / (beta + 1)) / 2  # pU, pR
+a = b * beta  # pD, pL
 
 
 class App:
@@ -36,32 +36,27 @@ class App:
             if agentPosition[0] != 0:
                 mat[agentPosition[0] - 1][agentPosition[1]] = 1
             else:
-                App.moveAgent(mat, agentPosition, 0, 1/4, 1/4, 1/2)
+                App.moveAgent(mat, agentPosition, 0, b, b, 2 * a)
         elif pU < r <= pU + pD:
             if agentPosition[0] != matrixSize - 1:
                 mat[agentPosition[0] + 1][agentPosition[1]] = 1
             else:
-                App.moveAgent(mat, agentPosition, 1/4, 0, 1/2, 1/4)
+                App.moveAgent(mat, agentPosition, a, 0, 2 * b, a)
         elif pU + pD < r <= pU + pD + pL:
             if agentPosition[1] != 0:
                 mat[agentPosition[0]][agentPosition[1] - 1] = 1
             else:
-                App.moveAgent(mat, agentPosition, 1/4, 1/2, 0, 1/4)
+                App.moveAgent(mat, agentPosition, a, 2 * b, 0, a)
         elif pU + pD + pL < r <= pU + pD + pL + pR:
             if agentPosition[1] != matrixSize - 1:
                 mat[agentPosition[0]][agentPosition[1] + 1] = 1
             else:
-                App.moveAgent(mat, agentPosition, 1/2, 1/4, 1/4, 0)
+                App.moveAgent(mat, agentPosition, 2 * a, b, b, 0)
         return mat
 
     def print2D(mat):
         for row in mat:
             print(row)
-
-    def plot(agentPosition):
-        xPoints.append(agentPosition[1])
-        yPoints.append(-agentPosition[0])
-        plt.scatter(xPoints, yPoints)
 
     @staticmethod
     def main():
@@ -73,7 +68,7 @@ class App:
             while agentPosition != [0, matrixSize - 1]:
                 agentPosition = App.getAgentPosition(mat)
                 mat[agentPosition[0]][agentPosition[1]] = 0
-                mat = App.moveAgent(mat, agentPosition, 1/4, 1/4, 1/4, 1/4)
+                mat = App.moveAgent(mat, agentPosition, a, b, b, a)
                 runtime = runtime + 1
                 agentPosition = App.getAgentPosition(mat)
                 # print(str(agentPosition[0]) + ", " + str(agentPosition[1]) + ", " + str(runtime))
