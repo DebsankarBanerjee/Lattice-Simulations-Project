2536b8611992ca92ce59de7f6aac5a32e894aa39
SambodhS <118314865+SambodhS@users.noreply.github.com>
2022-12-30 10:20:24 -0500

    Add files via upload

--- InteractiveRandomWalk.py
+++ InteractiveRandomWalk.py
@@ -1,5 +1,5 @@
 # can make function setProbabilities and then return array with a and b values if current beta setup not working
-# 1421 and 1422 runtimes cause overflow and invalid value errors leading to agent snapping to top left
+# 1421 and 1422 runtimes and omega 1 cause overflow and invalid value errors leading to agent snapping to top left
 
 import random
 import math
@@ -8,9 +8,9 @@ import numpy as np
 
 matrixSize = 41  # gets you a 21x21 matrix
 iterations = 1
-runtime = 1000
-beta = 1  # bias towards up and right
-omega = 0.1  # omega > 1
+runtime = 1
+beta = 10  # bias towards up and right
+omega = 1
 alpha = 1  # 1 for attracting random-walk, -1 for repulsing
 
 b = (1 / (beta + 1)) / 2  # pU, pR
@@ -23,7 +23,7 @@ class App:
         mat = [[0] * matrixSize for _ in range(matrixSize)]
         # mat[random.randint(1, matrixSize - 1)][random.randint(1, matrixSize - 1)] = 1
         mat[math.floor(matrixSize / 2)][math.floor(matrixSize / 2)] = 1
-        # mat[matrixSize - 1][0] = 1
+        # mat[0][matrixSize - 1] = 1
         return mat
 
     def getAgentPosition(mat):
@@ -44,28 +44,28 @@ class App:
         weight = np.exp(alpha * V)
         return weight
 
-    def moveAgent(mat, agentPosition, pU, pD, pL, pR):
+    def moveAgent(mat, agentPosition, pU, pD, pL, pR, wU, wD, wL, wR):
         r = random.uniform(0, 1)
         if r < pU:
             if agentPosition[0] != 0:
                 mat[agentPosition[0] - 1][agentPosition[1]] = 1
             else:
-                App.moveAgent(mat, agentPosition, 0, b, b, 2 * a)
+                App.moveAgent(mat, agentPosition, 0, (wD * b) / (wD * b + wL * b + wR * a), (wL * b) / (wD * b + wL * b + wR * a), (wR * a) / (wD * b + wL * b + wR * a), wU, wD, wL, wR)
         elif pU < r <= pU + pD:
             if agentPosition[0] != matrixSize - 1:
                 mat[agentPosition[0] + 1][agentPosition[1]] = 1
             else:
-                App.moveAgent(mat, agentPosition, a, 0, 2 * b, a)
+                App.moveAgent(mat, agentPosition, (wU * a) / (wU * a + wL * b + wR * a), 0, (wL * b) / (wU * a + wL * b + wR * a), (wR * a) / (wU * a + wL * b + wR * a), wU, wD, wL, wR)
         elif pU + pD < r <= pU + pD + pL:
             if agentPosition[1] != 0:
                 mat[agentPosition[0]][agentPosition[1] - 1] = 1
             else:
-                App.moveAgent(mat, agentPosition, a, 2 * b, 0, a)
+                App.moveAgent(mat, agentPosition, (wU * a) / (wU * a + wD * b + wR * a), (wD * b) / (wU * a + wD * b + wR * a), 0, (wR * a) / (wU * a + wD * b + wR * a), wU, wD, wL, wR)
         elif pU + pD + pL < r <= pU + pD + pL + pR:
             if agentPosition[1] != matrixSize - 1:
                 mat[agentPosition[0]][agentPosition[1] + 1] = 1
             else:
-                App.moveAgent(mat, agentPosition, 2 * a, b, b, 0)
+                App.moveAgent(mat, agentPosition, (wU * a) / (wU * a + wD * b + wL * b), (wD * b) / (wU * a + wD * b + wL * b), (wL * b) / (wU * a + wD * b + wL * b), 0, wU, wD, wL, wR)
         return mat
 
     def print2D(mat):
@@ -98,9 +98,8 @@ class App:
                     wR = App.weights(mem[agentPosition[0]][agentPosition[1] + 1])
                 except IndexError:
                     wR = 0
-                sum = (a * wU) + (b * wD) + (b * wL) + (a * wR)
                 mat[agentPosition[0]][agentPosition[1]] = 0
-                mat = App.moveAgent(mat, agentPosition, a * (wU / sum), b * (wD / sum), b * (wL / sum), a * (wR / sum))
+                mat = App.moveAgent(mat, agentPosition, a * (wU / ((a * wU) + (b * wD) + (b * wL) + (a * wR))), b * (wD / ((a * wU) + (b * wD) + (b * wL) + (a * wR))), b * (wL / ((a * wU) + (b * wD) + (b * wL) + (a * wR))), a * (wR / ((a * wU) + (b * wD) + (b * wL) + (a * wR))), wU, wR, wD, wL)
                 mem[agentPosition[0]][agentPosition[1]] = mem[agentPosition[0]][agentPosition[1]] + 1
                 # print(str(agentPosition[0]) + ", " + str(agentPosition[1]) + ", " + str(runtime))
                 # App.print2D(mat)
@@ -110,6 +109,7 @@ class App:
             # print(runtime)
             plt.imshow(mem, vmin=0, vmax=10)
             plt.colorbar()
+            plt.title("alpha = " + str(alpha) + ", omega = " + str(omega) + ", beta = " + str(beta))
             plt.show()
 
 
